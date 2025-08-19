0e52feaa34d65c5c3b9059e01ce4d71faba30bb5
vais2004 <vkawale2004@gmail.com>
2025-08-19 11:51:00 +0530

    update

--- models/students.model.js
+++ models/students.model.js
@@ -1,15 +1,29 @@
+// const mongoose = require('mongoose');
+
+// const studentSchema = new mongoose.Schema({
+//   name: String,
+//   age: Number,
+//   gender: String,
+//   marks: Number,
+//   attendance: Number,
+//   grade: String,
+// });
+
+// const Student = mongoose.model("Student", studentSchema);
+
+
+// module.exports = { Student };
 const mongoose = require('mongoose');
 
 const studentSchema = new mongoose.Schema({
-  name: String,
-  age: Number,
-  gender: String,
-  marks: Number,
-  attendance: Number,
-  grade: String,
+  name: { type: String, required: true },
+  age: { type: Number, required: true },
+  grade: { type: String, required: true },
+  gender: { type: String, default: '' },
+  attendance: { type: Number, default: 0 },
+  marks: { type: Number, default: 0 },
 });
 
 const Student = mongoose.model("Student", studentSchema);
-
 
 module.exports = { Student };
